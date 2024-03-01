94081cecba4aad4225048bdbae95b05e604e19b8
Said Khail <saidkhail@hotmail.com>
2024-03-01 18:03:27 +0000

    Updated files

---
+++ reactPortfolio/src/components/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+
+export const Header = () => {
+  return (
+    <div>
+      <Navbar expand='lg' className='bg-body-tertiary'>
+        <Container>
+          <Navbar.Brand href='#home'>React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='me-auto'>
+              {/* This NavDropdown is hidden on large screens */}
+              <NavDropdown title='Dropdown' id='basic-nav-dropdown' hidden lg>
+                <NavDropdown.Item href='#action/3.1'>Profile</NavDropdown.Item>
+                <NavDropdown.Item href='#action/3.2'>About-Me</NavDropdown.Item>
+                <NavDropdown.Item href='#action/3.3'>Projects</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href='#action/3.4'>Contact-Me</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  )
+}
+
+export default Header
---
+++ reactPortfolio/src/components/Homepage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Homepage = () => {
+  return <div className='d-flex justify-content-center'></div>
+}
+
+export default Homepage
