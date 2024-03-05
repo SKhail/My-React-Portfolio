40023603ae43e3553877fff1ed2ea9e8a5611205
Said Khail <saidkhail@hotmail.com>
2024-03-05 18:29:29 +0000

    Added Carousel for the project section

--- reactPortfolio/src/components/Projects.jsx
+++ reactPortfolio/src/components/Projects.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
-import { Container, Row, Col, Button, Card } from 'react-bootstrap'
+import { Container, Row, Col, Button, Card, Carousel } from 'react-bootstrap'
 import reactPortfolio from '../data/App.json'
-import projects from '../data/App.json'
 
 const Projects = () => {
   console.log('data: ', reactPortfolio)
@@ -11,9 +10,9 @@ const Projects = () => {
   return (
     <Container>
       <h2 className='mb-4'>Projects</h2>
-      <Row className='rol-cols-1 row-cols-md-2 row-cols-lg-3 g-4'>
+      <Carousel>
         {projects.map((item, index) => (
-          <Col key={index}>
+          <Carousel.Item key={index}>
             <Card className='h-100'>
               <Card.Img variant='top' src={item.src} className='project-img' />
               <Card.Body>
@@ -24,9 +23,9 @@ const Projects = () => {
                 </Button>
               </Card.Body>
             </Card>
-          </Col>
+          </Carousel.Item>
         ))}
-      </Row>
+      </Carousel>
     </Container>
   )
 }
